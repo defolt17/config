1356b76ed5d1d477e5717dd3cadc340863e7b684
defolt17 <defolt_17@mail.ru>
2020-05-20 14:09:09 +0300

    Portrait left->right

--- portrait.sh
+++
@@ -1,1 +0,0 @@
-xinput set-prop "bcm5974" "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1 && xrandr -o left
---
+++ scripts/portrait.sh
@@ -0,0 +1,1 @@
+xinput set-prop "bcm5974" "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1 && xrandr -o right
